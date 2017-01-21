82768d4af22b1e46d56b982a418757fe5e98e819
Mod <boningga@usc.edu>
2017-01-21 15:22:44 -0800

    List all music

--- server/server.py
+++ server/server.py
@@ -1,10 +1,19 @@
 from flask import Flask, url_for
-from server.xmlparser import *
+from .xmlparser import *
+from os import listdir
+import json
 app = Flask(__name__)
+
+scoreNames = [m for m in listdir('test/testCases/')]
+print(scoreNames)
 
 @app.route('/api/musicscores/<scorename>')
 def api_score(scorename):
     return generateJson(scorename)
+
+@app.route('/api/musicscores/')
+def score_list():
+    return json.dumps(scoreNames)
 
 if __name__ == '__main__':
     app.run()
