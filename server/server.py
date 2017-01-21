57ab5c16808df1efee0a332a057eca7b4754071a
Mod <boningga@usc.edu>
2017-01-21 15:48:30 -0800

    1.2
    Done

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, request
 from .xmlparser import *
 from os import listdir
 import json
@@ -11,9 +11,18 @@ scoreNames = [m for m in listdir('test/testCases/')]
 def api_score(scorename):
     return generateJson(scorename)
 
-@app.route('/api/musicscores/')
-def score_list():
-    return json.dumps(scoreNames)
+
+
+@app.route('/api/musicscores')
+def api_search():
+    if 'keyword' in request.args:
+        rList = []
+        for m in scoreNames:
+            if request.args['keyword'].lower() in m.lower():
+                rList.append(m)
+        return json.dumps(rList)
+    else:
+        return json.dumps(scoreNames)
 
 if __name__ == '__main__':
     app.run()
