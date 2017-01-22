9cb0d429778d0eaba979c25286b55bddd5cf8826
Mod <boningga@usc.edu>
2017-01-21 20:50:51 -0800

    Minor change

--- server/xmlparser.py
+++ server/xmlparser.py
@@ -151,7 +151,7 @@ def generateJson(scoreName):
                         m_text[int(_staff) - 1][-1] += note_text
 
                     else:
-                        note_text = _type + " " + _step + _octave
+                        note_text = _type + " " + _step + " " + _octave
                         if note.find('dot') is not None:
                             note_text = "dotted " + note_text
                         if note.find('grace') is not None:
