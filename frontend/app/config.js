8e0216b5fe5abbcfc138961db33432527c0e4a92
Jamie <wangqishere@gmail.com>
2017-03-23 16:22:04 -0700

    delete the stupid lock, use only chinese

--- frontend/app/config.js
+++ frontend/app/config.js
@@ -2,8 +2,7 @@ angular.module('myApp')
   .service('config', [function() {
       return {
           api : {
-              music_score : "http://duyuepu.com:5000/api/language/english"
-          },
-          ready : false
+              music_score : "http://duyuepu.com:5000/api/language/chinese"
+          }
       };
   }]);
